9a271135bea950995720f1fa612af1e84a379c15
Kingsley Amoke <klordbravo@gmail.com>
2025-03-14 09:04:11 +0100

    description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "My Plug",
   description:
-    "Connect with the right opportunities on [Your App Name]! Whether you're a job seeker looking for your next career move or an employer searching for top talent, our platform makes hiring fast and easy. Plus, buy and sell products effortlessly with our secure marketplace. Join thousands of users today and experience the ultimate hub for jobs and commerce!",
+    "Connect with the right opportunities on MyPlug! Whether you're a job seeker looking for your next career move or an employer searching for top talent, our platform makes hiring fast and easy. Plus, buy and sell products effortlessly with our secure marketplace. Join thousands of users today and experience the ultimate hub for jobs and commerce!",
 };
 
 export default function RootLayout({
